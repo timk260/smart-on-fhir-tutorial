c7d803fba7956fdfd9bbea551374e20484ad1dc5
timk260 <timk260@users.noreply.github.com>
2018-03-28 10:30:55 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -26,7 +26,7 @@
 	$.when(pt, enc).done(function(patient, enc) {
 		$('#holder').show();
 		$('#loading').hide();
-		$('#enc').html(JSON.stringify(Object.keys(enc.ResourceType)));
+		$('#enc').html('val='+JSON.stringify(enc.ResourceType));
 	});
 
 		  /*
